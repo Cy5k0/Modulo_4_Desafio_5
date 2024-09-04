2279f6ec621deffc74877df8dde4bafe0a288d38
Cy5k0 <francisco.colomer@gmail.com>
2024-09-03 22:00:57 -0400

    update error

--- error.py
+++ error.py
@@ -1,11 +1,16 @@
 class DimensionError(Exception):
-    def __init__(self, mensaje: str, dimension: int, maximo: int) -> None:
+    def __init__(self, mensaje, dimension=None, maximo=None):
+        super().__init__(mensaje)
         self.mensaje = mensaje
         self.dimension = dimension
         self.maximo = maximo
-        super().__init__(mensaje)
 
     def __str__(self):
-        if self.dimension is not None and self.maximo is not None:
-            return f"{self.mensaje}: {self.dimension} (máximo permitido: {self.maximo})"
-        return super().__str__()
+        if self.dimension is None and self.maximo is None:
+            return super().__str__()
+        elif self.dimension is not None and self.maximo is not None:
+            return f"{self.mensaje}: {self.dimension} excede el máximo de {self.maximo}"
+        elif self.dimension is not None:
+            return f"{self.mensaje}: dimensión {self.dimension} no válida"
+        else:
+            return f"{self.mensaje}: máximo {self.maximo}"
--- foto.py
+++ foto.py
@@ -1,6 +1,7 @@
 from error import DimensionError
 
-class Foto():
+
+class Foto:
     MAX = 2500
 
     def __init__(self, ancho: int, alto: int, ruta: str) -> None:
@@ -13,7 +14,7 @@ class Foto():
         return self.__ancho
 
     @ancho.setter
-    def ancho(self, ancho:int) -> None:
+    def ancho(self, ancho: int) -> None:
         if ancho < 1 or ancho > Foto.MAX:
             raise DimensionError("Ancho no válido", ancho, Foto.MAX)
         self.__ancho = ancho
@@ -23,26 +24,23 @@ class Foto():
         return self.__alto
 
     @alto.setter
-    def alto(self, alto:int) -> None:
+    def alto(self, alto: int) -> None:
         if alto < 1 or alto > Foto.MAX:
             raise DimensionError("Alto no válido", alto, Foto.MAX)
         else:
             self.__alto = alto
 
 
-
-
-
-ruta=input("Ingrese ruta de la imagen: ")
-int_ancho=int(input("Ingrese ancho de la imagen: "))
-int_alto=int(input("Ingrese alto de la imagen: "))
+ruta = input("Ingrese ruta de la imagen: ")
+int_ancho = int(input("Ingrese ancho de la imagen: "))
+int_alto = int(input("Ingrese alto de la imagen: "))
 fotos = Foto(int_alto, int_ancho, ruta)
 print(fotos.alto)
 print(fotos.ancho)
-int_ancho=int(input("Ingrese nuevo ancho de la imagen: "))
-int_alto=int(input("Ingrese nuevo alto de la imagen: "))
+int_ancho = int(input("Ingrese nuevo ancho de la imagen: "))
+int_alto = int(input("Ingrese nuevo alto de la imagen: "))
 
-fotos.ancho=int_ancho
+fotos.ancho = int_ancho
 print(fotos.ancho)
-fotos.alto=int_alto
+fotos.alto = int_alto
 print(fotos.alto)
